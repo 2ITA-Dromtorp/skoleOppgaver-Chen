26a02663308e0f458bd2116d32a618cf6823655a
helpMeWithAName <chen.alexander.fuglestad@gmail.com>
2024-02-08 14:52:29 +0100

    karaoki hur hur

--- karaoki-app/server/index.js
+++ karaoki-app/server/index.js
@@ -1,23 +1,25 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/karaoki", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const Item = require("./models/item"); // Create the Item model
-
-app.get("/api/items", async (req, res) => {
-  try {
-    const items = await Item.find();
-    res.json(items);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Server Error");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://chenalexanderfuglestad:8mM18XRvxzkXj3Vu@cluster0.bjn32lb.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
 });
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//express.json({limit: '20mb'})
